f4b9eaee5ec2cbe1f8a3ced297535d1388515c17
Nikolay <zorinov.nikolai@yandex.ru>
2018-10-18 18:02:55 +0300

    add create, edit, delete process

--- hello/urls.py
+++ hello/urls.py
@@ -6,6 +6,8 @@ from helloworld import views
 urlpatterns = [
     path('', views.index),
     path('create/', views.create),
+    path('edit/<int:id>/', views.edit),
+    path('delete/<int:id>/', views.delete),
     path('process/', views.process),
     path('form/', views.form),
     re_path(r'', views.users),
--- helloworld/views.py
+++ helloworld/views.py
@@ -1,15 +1,13 @@
-from django.http import HttpResponse
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import render
 
-from .forms import UserForm
-from .models import Process
+from helloworld.forms import UserForm
+from helloworld.models import Process
 
 
 def index(request):
     processes = Process.objects.all()
     return render(request, "index.html", {"processes": processes})
-
 
 def create(request):
     if request.method == "POST":
@@ -19,6 +17,28 @@ def create(request):
         tom.user = request.POST.get("user")
         tom.save()
     return HttpResponseRedirect("/")
+
+def edit(request, id):
+    try:
+        process = Process.objects.get(id=id)
+        if request.method == "POST":
+            process.proc_id = request.POST.get("proc_id")
+            process.name = request.POST.get("name")
+            process.user = request.POST.get("user")
+            process.save()
+            return HttpResponseRedirect("/")
+        else:
+            return render(request, "edit.html", {"process": process})
+    except Process.DoesNotExist:
+        return HttpResponseNotFound("<h2>Process not found</h2>")
+
+def delete(request, id):
+    try:
+        process = Process.objects.get(id=id)
+        process.delete()
+        return HttpResponseRedirect("/")
+    except Process.DoesNotExist:
+        return HttpResponseNotFound("<h2>Process not found</h2>")
 
 
 def process(request):
